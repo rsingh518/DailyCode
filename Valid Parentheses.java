35d1b3c331c336d46babf1fc40dba6cb31adbd4e
Rohit <rohitsinghcs.20@gmail.com>
2023-08-13 00:26:20 +0530

    20. Valid Parentheses

---
+++ Valid Parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+//https://leetcode.com/problems/valid-parentheses/
+
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+        
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char curr = s.charAt(i);
+            
+            if(map.containsKey(curr)){
+                char pop = stack.size() !=0 ? stack.pop() : '#';
+                if(pop != map.get(curr)){
+                    return false;
+                }
+            }else{
+                stack.push(curr);
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
